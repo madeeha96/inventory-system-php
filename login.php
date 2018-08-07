f12c89af23967087117b7441a480741601fc687f
Yasser Alattas <38978501+yalattas@users.noreply.github.com>
2018-08-07 04:23:32 +0300

    Update login.php
    Check All my modification. I insert it non-executable (As comments)
    You are using old technique (Old school)
    You need to use frameworks like (Yii, Laravel or etc.) and you can use Firebase backend as well
    
    Good luck

--- login.php
+++ login.php
@@ -11,13 +11,13 @@ $Success = "FALSE";
 if(!empty($_POST))
 {
 $servername = "localhost";
-$username = "root";
+$username = "root";							//Can you expot your DB and send the file to me. So I can run the App ?
 $password = "";
 $db = "Inventory";
 $con = mysqli_connect($servername,$username ,$password ,$db);
 if($con)
 {
-	$sql = "SELECT * FROM login";
+	$sql = "SELECT * FROM login";					//You are Typing a direct query, If you used an approach like this. I can hack your system easily just I need to send query to your system
   	$result = mysqli_query($con, $sql); 
 	while($row = $result->fetch_assoc()) 
 	{
@@ -29,8 +29,59 @@ if($con)
 		}
 	}
 }
-
-
+//------------------------------------------------------------------------------------------------------------------------
+        
+       //Pls find the below login code. you can review it only. it's week login but stronger than you
+       //You must use a frame work or pior object oriented PHP application. NOT old school (HTML with PHP)
+                    if (isset($_POST["LoginButt"])) {
+                        $con = new mysqli('localhost', 'username', 'Password', 'DB_name');
+                        if ($con->error) {
+                            echo "connection failed";
+                        } else {
+                            $userID = $con->query("Call LOGIN('" . $_POST['inputEmail'] . "','" . $_POST['inputPassword'] . "')");
+                            if ($userID == null)
+                                die("Something goes wrong, Please wait few minutes<br>");
+                            if (!$userID = $userID->fetch_object()) {
+                                die("Username or Password are wrong");
+                            } else {
+                                $Security_Level = $userID->SECURITY_LEVEL;
+                                $userID = $userID->USER_ID;
+                                //Login Done successfull without checking the company activation status
+                                $con = new mysqli('localhost', 'username', 'Password', 'DB_name');
+                                $activationStatus = $con->query("CALL checkCompanyActivationThrowUser('" . $userID . "')");
+                                if (!$activationStatus = $activationStatus->fetch_object()) {
+                                    die("Error by retrieving the company status");
+                                } else {
+                                    $activationStatus = $activationStatus->ACTIVATION;
+                                    if ($activationStatus == "DISABLED") {
+                                        echo "The company accounts are disabled, check your administrator";
+                                    } else if ($activationStatus == "ACTIVE") {
+                                        // write code after signin and checking the company status is successfully done ------------------------
+                                        if (isset($_POST['RememberMeCheck'])) {
+                                            setcookie('userID', $userID, time() + 10000);
+                                            setcookie('Email', $Email, time() + 10000);
+                                            setcookie('Passw', $Password, time() + 10000);
+                                        }
+                                        $_SESSION['userID'] = $userID;
+                                        $_SESSION['SECURITY_LEVEL'] = $Security_Level;
+                                        header("location: ../AMP/users/Level_" . $_SESSION['SECURITY_LEVEL'] . ".php");
+                                    } else {
+                                        echo "The system can not defined activation status, check support team";
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    if (isset($_COOKIE['Email']) && isset($_COOKIE['Passw'])) {
+                        $Email = $_COOKIE['Email'];
+                        $Password = $_COOKIE['Passw'];
+                        echo "<script>
+                                document.getElementById('Emailtxt').value = '$Email';
+                                document.getElementById('PassTxt').value = '$Password';
+                            </script>";
+                    }
+                    
+//-------------------------------------------------------------------------------------------------------------------------
 
 if($Success == "TRUE")
 {
